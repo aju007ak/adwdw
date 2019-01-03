9807aac4e62d6f82647322a3eafe3af0fb2c8819
aju007ak <john.ajith95ak@gmail.com>
2019-01-03 19:32:58 +0530

    Update ghhg.c

--- ghhg.c
+++ ghhg.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, sum = 0;
+    int n,i,s = 0;
     
     printf("Enter a positive integer: ");
     scanf("%d",&n);
 
     for(i=1; i <= n; ++i)
     {
-        sum += i;   
+        s+= i;   
     }
 
-    printf("Sum = %d",sum);
+    printf("Sum = %d",s);
 
     return 0;
 }
