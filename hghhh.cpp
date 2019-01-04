54b9cc6cf18712bbbbbbb0d8122cec04a1dc317f
aju007ak <john.ajith95ak@gmail.com>
2019-01-04 12:13:49 +0530

    Create hghhh.cpp

---
+++ hghhh.cpp
@@ -0,0 +1,33 @@
+
+
+#include <iostream> 
+using namespace std; 
+  
+int minCost(string& s) 
+{ 
+
+    bool alphabets[26] = { false }; 
+  
+   
+    for (int i = 0; i < s.size(); i++) 
+        alphabets[s[i] - 97] = true; 
+  
+    
+    int count = 0; 
+    for (int i = 0; i < 26; i++) 
+        if (alphabets[i]) 
+            count++; 
+  
+    return count; 
+} 
+  
+int main() 
+{ 
+   
+    string s = "geeksforgeeks"; 
+  
+    cout << "Total cost to construct "
+         << s << " is " << minCost; 
+  
+    return 0; 
+} 
