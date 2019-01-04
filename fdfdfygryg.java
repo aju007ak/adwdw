a9afd17e415f6f719115ba503ab9b40a732c00f2
aju007ak <john.ajith95ak@gmail.com>
2019-01-04 11:59:54 +0530

    Create fdfdfygryg.java

---
+++ fdfdfygryg.java
@@ -0,0 +1,52 @@
+import java.io.*; 
+import java.util.*; 
+  
+class GFG{ 
+      
+
+    static int fact(int n) 
+    { 
+        return (n <= 1)? 1 :n * fact(n-1); 
+    } 
+   
+    
+    static int findSmallerInRight(String str, int low, 
+                                            int high) 
+    { 
+        int countRight = 0, i; 
+   
+        for (i = low + 1; i <= high; ++i) 
+            if (str.charAt(i) < str.charAt(low)) 
+                ++countRight; 
+   
+        return countRight; 
+    } 
+   
+    
+    static int findRank (String str) 
+    { 
+        int len = str.length(); 
+        int mul = fact(len); 
+        int rank = 1; 
+        int countRight; 
+   
+        for (int i = 0; i < len; ++i) 
+        { 
+            mul /= len - i; 
+   
+          
+            countRight = findSmallerInRight(str, i, len-1); 
+   
+            rank += countRight * mul ; 
+        } 
+          
+        return rank; 
+    } 
+
+    public static void main(String[] args) 
+    { 
+        String str = "string"; 
+        System.out.println (findRank(str)); 
+    } 
+} 
+  
