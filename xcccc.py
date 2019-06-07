5fd8cdb41b387f3473b78d7d179b661198a043b1
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 16:16:30 +0530

    Update xcccc.py

--- xcccc.py
+++ xcccc.py
@@ -1,33 +1,41 @@
-def nextGreatest(arr): 
-  
-    size = len(arr) 
-  
-   
-    max_from_right = arr[size-1] 
-  
-   
-    arr[size-1] = -1
-  
+import sys, string, math
 
-    for i in range(size-2,-1,-1): 
-  
-     
-        temp = arr[i] 
-  
-       
-        arr[i]=max_from_right 
-  
-     
-        if max_from_right< temp: 
-            max_from_right=temp 
-  
+n = int(input())
+L1 = [ int(x) for x in input().split()]
+Lno = []
+Ldup = []
+Lin = []
+for i in range(1,n+1) :
+    if i not in L1 :
+        Lno.append(i)
+for i in L1 :
+    if L1.count(i) > 1 and i not in Ldup :
+        Ldup.append(i)
+for i in range(0,n) :
+    if L1[i] in Ldup :
+        Lin.append(i)
+cnt = len(Lno)
+#print('Lno=',Lno)
+#print('Ldup=',Ldup)
+#print('Lin=',Lin)
 
-def printArray(arr): 
-    for i in range(0,len(arr)): 
-        print arr[i], 
-  
+for i in range(0,n) :
+    if len(Lno) == 0 :
+        break
+    if i in Lin :
+        if i == Lin[0] :
+            if Lno[0] < L1[i] :
+                L1[i] = Lno.pop(0)
+                Lin.pop(0)
+            elif L1[i] in Ldup :
+                Lin.pop(0)
+                Ldup.remove(L1[i])
+            else :
+                L1[i] = Lno.pop(0)
+                Lin.pop(0)
 
-arr = [16, 17, 4, 3, 5, 2] 
-nextGreatest(arr) 
-print "Modified array is"
+
+print(cnt)
+print(*L1)
+fied array is"
 printArray(arr) 
