255153af318c433a1f224faee889e2532e499649
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 14:12:40 +0530

    Update fdsfsf.py

--- fdsfsf.py
+++ fdsfsf.py
@@ -1,17 +1,12 @@
-from numpy import zeros
- 
-inp = zeros(shape=(6,8),dtype=int)
-inp[0,7]=1
-inp[3,1:4] = 1
- 
-inp_new = 0*inp
-print(inp)
- 
-P = inp.shape[0]
-Q = inp.shape[1]
- 
-print(P, Q)
- 
-for p in range(1,P-1):
-    for q in range(1, Q-1):
-        inp_new[p,q] = inp[p+1, q]+inp[p-1, q]+ inp[p, q+1] + inp[p, q-1]+inp[p+1,q+1]+inp[p+1,q- 1]+inp[p+1,q-1]+inp[p-1,q- 1]
+import sys,string
+a = int(input())
+L = [ int(x) for x in input().split()]
+a = len(L)
+if a==1 :
+    print(1)
+    sys.exit()
+cnt = 0
+for i in range(1,a-1) :
+    if ((L[i] > L[i-1]) and (L[i] > L[i+1])) or ((L[i] < L[i-1]) and (L[i] < L[i+1])):
+        cnt += 1
+print(cnt)
