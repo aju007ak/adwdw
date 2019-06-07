a3b45a68149339c87af90ad794bc73c34edfbcfc
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 12:08:39 +0530

    Update jhjhjhj.py

--- jhjhjhj.py
+++ jhjhjhj.py
@@ -1,31 +1,12 @@
-def printCombination(arr, n, r): 
-    
-    data = [0]*r; 
-  
-   
-    combinationUtil(arr, data, 0,  
-                    n - 1, 0, r); 
-  
-
-def combinationUtil(arr, data, start,  
-                    end, index, r): 
-  
-    if (index == r): 
-        for j in range(r): 
-            print(data[j], end = " "); 
-        print(); 
-        return; 
-  
-    
-    i = start;  
-    while(i <= end and end - i + 1 >= r - index): 
-        data[index] = arr[i]; 
-        combinationUtil(arr, data, i + 1,  
-                        end, index + 1, r); 
-        i += 1; 
-  
-
-arr = [1, 2, 3, 4, 5]; 
-r = 3; 
-n = len(arr); 
-printCombination(arr, n, r); 
+import sys, string, math
+n = int(input())
+k = 2**n
+L = []
+for i in range(0,k) :
+    s = bin(i)[2:]
+    j = len(s)
+    if j < n :
+        s = '0' * (n-j) + s
+    L.append(s)
+for i in range(0,len(L)) :
+    print(L[i])
