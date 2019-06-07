31c4b2b2f503d098485f340c768b508a158a2551
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 16:15:54 +0530

    Update qqw.py

--- qqw.py
+++ qqw.py
@@ -1,47 +1,35 @@
-def hasArrayTwoCandidates(A,arr_size,sum): 
-      
-   
-    quickSort(A,0,arr_size-1) 
-    l = 0
-    r = arr_size-1
-      
-   
-    while l<r: 
-        if (A[l] + A[r] == sum): 
-            return 1
-        elif (A[l] + A[r] < sum): 
-            l += 1
-        else: 
-            r -= 1
-    return 0
-  
- 
-def quickSort(A, si, ei): 
-    if si < ei: 
-        pi=partition(A,si,ei) 
-        quickSort(A,si,pi-1) 
-        quickSort(A,pi+1,ei) 
-  
-
-def partition(A, si, ei): 
-    x = A[ei] 
-    i = (si-1) 
-    for j in range(si,ei): 
-        if A[j] <= x: 
-            i += 1
-              
-      
-            A[i], A[j] = A[j], A[i] 
-  
-        A[i+1], A[ei] = A[ei], A[i+1] 
-          
-    return i+1
-      
-  
-
-A = [1,4,45,6,10,-8] 
-n = 16
-if (hasArrayTwoCandidates(A, len(A), n)): 
-    print("Array has two elements with the given sum") 
-else: 
-    print("Array doesn't have two elements with the given sum") 
+import sys,string
+n0,n1 = input().split()
+n0,n1 = int(n0),int(n1)
+if n1 > (2+2*n0) :
+    print(-1)
+    sys.exit()
+if n0 > n1-1 :
+    print(-1)
+    sys.exit()
+s = ''
+if n1-n0 == 1 :
+    s = '1'+'01'*n0
+    print(s)
+    sys.exit()
+while n1 > n0 and n0 > 0:
+    if n1-n0 == 1 :
+        break
+    n1 -= 2
+    n0 -= 1
+    #print(n1,n0)
+    s += '110'
+if n1-n0 == 1 :
+    s += '10'*n0+'1'
+    print(s)
+    sys.exit()
+if n1 > 2 and n0==0 :
+    print(-1)
+    sys.exit()
+elif n0==1 and n1==1 :
+    s += '0'+'1'*n1
+elif n0==1 and n1==2 :
+    s += '101'
+elif n0==0:
+    s += '1'*n1
+print(s)
