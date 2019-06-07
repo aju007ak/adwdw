e88991debccc42bd12b571df1de257aa867e9918
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 14:11:46 +0530

    Update fdfer3rdf.py

--- fdfer3rdf.py
+++ fdfer3rdf.py
@@ -1,11 +1,11 @@
 import sys,string
-n = int(input())
+a = int(input())
 L = [ int(x) for x in input().split()]
-n = len(L)
+a = len(L)
 cnt = 0
-for i in range(0,n-2) :
-    for j in range(i+1, n-1):
-        for k in range(j+1, n):
+for i in range(0,a-2) :
+    for j in range(i+1, a-1):
+        for k in range(j+1, a):
             if L[i] > L[j] > L[k] :
                 cnt += 1
 print(cnt)
