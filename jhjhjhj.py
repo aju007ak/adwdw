a5dbec1f6607bada7c46ef86bde6de667aaa0914
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 12:10:31 +0530

    Update jhjhjhj.py

--- jhjhjhj.py
+++ jhjhjhj.py
@@ -1,12 +1,13 @@
 import sys, string, math
-n = int(input())
-k = 2**n
-L = []
+a = int(input())
+k = 2**a
+D= []
 for i in range(0,k) :
     s = bin(i)[2:]
     j = len(s)
     if j < n :
-        s = '0' * (n-j) + s
-    L.append(s)
-for i in range(0,len(L)) :
-    print(L[i])
+        s = '0' * (a-j) + s
+    D.append(s)
+for i in range(0,len(D)) :
+    print(D[i])
+
