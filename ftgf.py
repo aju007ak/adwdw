86042334b47d2bef48218f6f438ebdbdd27f49c8
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 12:06:18 +0530

    Update ftgf.py

--- ftgf.py
+++ ftgf.py
@@ -1,15 +1,23 @@
-def sumOfMaximumOfSubsets(arr, N): 
+import sys, string, math
 
-    arr.sort(reverse = True) 
-  
- 
-    sum = arr[0] 
-    for i in range(1, N): 
-      
-        sum = 2 * sum + arr[i] 
-  
-    return sum
+# Function to return max sum such that no two elements are adjacent
+def find_max_sum(arr):
+    incl = 0
+    excl = 0
 
-arr = [3, 2, 5] 
-N = len(arr)  
-print(sumOfMaximumOfSubsets(arr, N)) 
+    for i in arr:
+        # Current max excluding i == C ? : opr
+        new_excl = excl if excl > incl else incl
+
+        # Current max including i
+        incl = excl + i
+        excl = new_excl
+
+        # return max of incl and excl
+    return (excl if excl > incl else incl)
+
+
+# Driver program to test above function
+n = int(input())
+L = [ int(x) for x in input().split()]
+print(find_max_sum(L))
