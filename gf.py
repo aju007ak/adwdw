8509c18829c8246e949c8b086692e28eab0422cb
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 11:55:45 +0530

    Update gf.py

--- gf.py
+++ gf.py
@@ -1,28 +1,18 @@
-def largestNumber(array): 
-      
-    extval, ans = [], "" 
-      
-    l = len(str(max(array))) + 1
-   
-    for i in array: 
-        temp = str(i) * l 
-       
-  
-        extval.append((temp[:l:], i)) 
-      
- 
-    extval.sort(reverse = True) 
-
-    for i in extval: 
-          
-    
-        ans += str(i[1]) 
-          
-    return ans 
-  
-
-a = [1, 34, 3, 98, 9, 76, 
-        45, 4, 12, 121] 
-  
-print(largestNumber(a)) 
+import sys, string, math
+def count_1(n) :
+    s = bin(n)[2:]
+    k = s.count('1')
+    return k
+n = int(input())
+L = [ int(x) for x in input().split()]
+#L2 = sorted(L, key=count_1,reverse=True)
+L2 = []
+for i in range(0,n) :
+    L2.append((count_1(L[i]),L[i]))
+#print(L2)
+L3 = sorted(L2, key=lambda x : (x[0],x[1]),reverse=True)
+#print(L3)
+L4 = [x[1] for x in L3 ]
+for i in range(0,len(L4)) :
+    print(L4[i])
   
